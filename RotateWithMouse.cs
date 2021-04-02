cb1e72c89b415b94b4c5f6c88218a731757332a4
Sanket Patel <48343143+Sanket2130@users.noreply.github.com>
2021-04-02 11:46:59 +0530

    Add files via upload

---
+++ RotateWithMouse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+
+public class RotateWithMouse : MonoBehaviour
+{
+    [SerializeField] private float turnSpeed = 3f;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Mouse X");
+        transform.Rotate(horizontal * turnSpeed * Vector3.up, Space.World);
+    }
+}
---
+++ jump.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+
+    public class jump : MonoBehaviour
+    {
+        private Rigidbody rigidbody;
+        [SerializeField] private float jumpForce = 300f;
+        private bool shouldJump;
+
+        private void Awake() => rigidbody = GetComponent<Rigidbody>();
+
+        private void Update()
+        {
+            if (shouldJump == false)
+                shouldJump = Input.GetKeyDown(KeyCode.Space);
+            
+        }
+
+        private void FixedUpdate()
+        {
+            if (shouldJump)
+            {
+                rigidbody.AddForce(jumpForce * Vector3.up);
+                shouldJump = false;
+            }
+        }
+    }
